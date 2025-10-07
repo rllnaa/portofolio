689b671cc47c89e54c7f0614b5fcf5ac9c62d746
rllnaa <r81195669@gmail.com>
2025-10-07 12:07:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const tombolSapa=document.getElementById('sapaButton');
 tombolSapa.addEventListener('click',function(){
-  alert('Haaiii!Arigatoooo All!);  
+  alert('Haaiii!Arigatoooo All!');  
 });
